9e1073d915ef13e214c42880b7b770a3bde85da6
Ashley <wheeliedaniel@gmail.com>
2026-01-16 23:08:41 +0000

    Updated remove setting to a more logical system

--- src/json_worker.py
+++ src/json_worker.py
@@ -17,31 +17,32 @@ async def json_worker(path: str, queue: asyncio.Queue[dict], user_data: dict) ->
             except FileNotFoundError:
                 data = {} # Set to empty if no file found
 
+            user_did = update["user_did"]
+
             # -- Get the update data
             if update.get("type") == "update":
-                user_did = update["user_did"]
                 update_data = {}
                 for key, value in update.items():
                     if key not in ["type", "user_did"]:
-                        if value == "!pop_entry": # Check if the value is a pop instruction
-                            if data.get(user_did, {}).get(key, None):
-                                data[user_did].pop(key) # Pop an entry if a pop command was recieved
-                        else:
-                            update_data[key] = value 
+                        update_data[key] = value 
                         
                 if user_did not in data:
                     data[user_did] = {} # Set to empty dict if user was not in the file
                 data[user_did].update(update_data)
-                user_data.clear()
-                user_data.update(data)
+            
+            elif update.get("type") == "remove":
+                key = update["setting"]
+
+                if data.get(user_did, {}).get(key, None):
+                    data[user_did].pop(key)
             
             # -- Delete a user's data
             elif update.get("type") == "delete":
-                user_did = update["user_did"]
                 if user_did in data:
                     del data[user_did]
-                    user_data.clear()
-                    user_data.update(data)
+
+            user_data.clear()
+            user_data.update(data)
 
             # -- Update the file
             with open(path, "w", encoding="utf-8") as f:
