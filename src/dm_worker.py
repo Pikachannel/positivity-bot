eded6793fe1c1612c1a005555f23b21978dc3c16
Ashley <wheeliedaniel@gmail.com>
2026-05-28 18:22:54 +0100

    Make dm_worker handle multiple bot instances

--- src/dm_worker.py
+++ src/dm_worker.py
@@ -6,11 +6,11 @@ from datetime import datetime, timezone, timedelta
 
 # -------- DmWorker Class --------
 class DmWorker:
-    def __init__(self, client: Client, command_manager: Any, json_queue: asyncio.Queue, account_did: str, check_interval: int = 300) -> None:
-        self.client = client
+    def __init__(self, bots: dict, command_manager: Any, json_queue: asyncio.Queue, account_dids: list, check_interval: int = 300) -> None:
+        self.bots = bots
         self.command_manager = command_manager
         self.json_queue = json_queue
-        self.account_did = account_did
+        self.account_dids = account_dids
         self.check_interval = check_interval
     
     # -------------
@@ -27,69 +27,72 @@ class DmWorker:
     # -------------
     # -- Check DMs sent to the bot
     async def check_dms(self) -> None:
-        # -- Get the messages sent to the bot
-        dm_client = self.client.with_bsky_chat_proxy()
-        dm = dm_client.chat.bsky.convo
+        # -- Run through each bot
+        for account_did in self.account_dids:
+            # -- Get the messages sent to the bot
+            client = self.bots[account_did]["client"]
+            dm_client = client.with_bsky_chat_proxy()
+            dm = dm_client.chat.bsky.convo
 
-        convo_list = dm.list_convos()
+            convo_list = dm.list_convos()
 
-        # -- Loop through all users dms
-        for convo in convo_list.convos:
-            # -- Extract message information
-            user_did = convo.last_message.sender.did
-            last_message = convo.last_message.text
-            sent_at = convo.last_message.sent_at
-            sent_at = datetime.fromisoformat(sent_at.replace("Z", "+00:00"))
+            # -- Loop through all users dms
+            for convo in convo_list.convos:
+                # -- Extract message information
+                user_did = convo.last_message.sender.did
+                last_message = convo.last_message.text
+                sent_at = convo.last_message.sent_at
+                sent_at = datetime.fromisoformat(sent_at.replace("Z", "+00:00"))
 
-            # -- Check if the message was sent in the last 5 minutes
-            now = datetime.now(timezone.utc)
-            if now - sent_at >= timedelta(minutes=5):
-                break
-            if user_did == self.account_did:
-                continue
+                # -- Check if the message was sent in the last 5 minutes
+                now = datetime.now(timezone.utc)
+                if now - sent_at >= timedelta(minutes=5):
+                    break
+                if user_did == account_did:
+                    continue
 
-            # -- Split the message into a prefix and param 
-            parts = last_message.split(maxsplit=1)
-            command = parts[0].lower()
-            param = parts[1] if len(parts) > 1 else None
+                # -- Split the message into a prefix and param 
+                parts = last_message.split(maxsplit=1)
+                command = parts[0].lower()
+                param = parts[1] if len(parts) > 1 else None
 
-            # Map to CommandManager methods
-            msg_to_send = None
-            facet = None
-            if command == "!nickname":
-                _, msg_to_send = await self.command_manager.update_nickname(user_did, param)
-            elif command == "!chance":
-                _, msg_to_send = await self.command_manager.chance(user_did, param)
-            elif command == "!interval":
-                _, msg_to_send = await self.command_manager.interval(user_did, param)
-            elif command == "!skip":
-                _, msg_to_send = await self.command_manager.skip_posts(user_did, param)
-            elif command == "!delete":
-                _, msg_to_send = await self.command_manager.delete_settings(user_did)
-            elif command == "!settings":
-                _, msg_to_send = await self.command_manager.view_settings(user_did)
-            elif command == "!help":
-                _, msg_to_send, facet = await self.command_manager.help()
-            else:
-                continue
-            
-            if msg_to_send:
-                if not facet:
-                    dm.send_message(
-                        models.ChatBskyConvoSendMessage.Data(
-                            convo_id=convo.id,
-                            message=models.ChatBskyConvoDefs.MessageInput(
-                                text=msg_to_send
-                            ),
-                        )
-                    )
+                # Map to CommandManager methods
+                msg_to_send = None
+                facet = None
+                if command == "!nickname":
+                    _, msg_to_send = await self.command_manager.update_nickname(user_did, param)
+                elif command == "!chance":
+                    _, msg_to_send = await self.command_manager.chance(user_did, param)
+                elif command == "!interval":
+                    _, msg_to_send = await self.command_manager.interval(user_did, param)
+                elif command == "!skip":
+                    _, msg_to_send = await self.command_manager.skip_posts(user_did, param)
+                elif command == "!delete":
+                    _, msg_to_send = await self.command_manager.delete_settings(user_did)
+                elif command == "!settings":
+                    _, msg_to_send = await self.command_manager.view_settings(user_did)
+                elif command == "!help":
+                    _, msg_to_send, facet = await self.command_manager.help()
                 else:
-                    dm.send_message(
-                        models.ChatBskyConvoSendMessage.Data(
-                            convo_id=convo.id,
-                            message=models.ChatBskyConvoDefs.MessageInput(
-                                text=msg_to_send,
-                                facets=[facet]
-                            ),
+                    continue
+                
+                if msg_to_send:
+                    if not facet:
+                        dm.send_message(
+                            models.ChatBskyConvoSendMessage.Data(
+                                convo_id=convo.id,
+                                message=models.ChatBskyConvoDefs.MessageInput(
+                                    text=msg_to_send
+                                ),
+                            )
                         )
-                    )
+                    else:
+                        dm.send_message(
+                            models.ChatBskyConvoSendMessage.Data(
+                                convo_id=convo.id,
+                                message=models.ChatBskyConvoDefs.MessageInput(
+                                    text=msg_to_send,
+                                    facets=[facet]
+                                ),
+                            )
+                        )
