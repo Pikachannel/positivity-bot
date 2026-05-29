8fb33d59f07ad0e613d7daf85721b855f9cf5d28
Ashley <wheeliedaniel@gmail.com>
2026-05-29 18:29:33 +0100

    Make json worker save data with a bot did as a key
    Also fixed a bug where removing settings that had a falasy value set by the user impossilbe

--- src/json_worker.py
+++ src/json_worker.py
@@ -17,29 +17,30 @@ async def json_worker(path: str, queue: asyncio.Queue[dict], user_data: dict) ->
             except FileNotFoundError:
                 data = {} # Set to empty if no file found
 
+            account_did = update.get("account_did")
             user_did = update["user_did"]
 
             # -- Get the update data
             if update.get("type") == "update":
                 update_data = {}
                 for key, value in update.items():
-                    if key not in ["type", "user_did"]:
+                    if key not in ["type", "user_did", "account_did"]:
                         update_data[key] = value 
                         
-                if user_did not in data:
-                    data[user_did] = {} # Set to empty dict if user was not in the file
-                data[user_did].update(update_data)
+                if user_did not in data[account_did]:
+                    data[account_did][user_did] = {} # Set to empty dict if user was not in the file
+                data[account_did][user_did].update(update_data)
             
             elif update.get("type") == "remove":
                 key = update["setting"]
 
-                if data.get(user_did, {}).get(key, None):
-                    data[user_did].pop(key)
+                if account_did in data and user_did in data[account_did] and key in data[account_did][user_did]:
+                    data[account_did][user_did].pop(key, None)
             
             # -- Delete a user's data
             elif update.get("type") == "delete":
-                if user_did in data:
-                    del data[user_did]
+                if user_did in data[account_did]:
+                    del data[account_did][user_did]
 
             user_data.clear()
             user_data.update(data)
@@ -51,3 +52,4 @@ async def json_worker(path: str, queue: asyncio.Queue[dict], user_data: dict) ->
             print(f"[JSON Worker] An error has occured, {e}")
         finally:
             queue.task_done() # Remove task from queue 
+
